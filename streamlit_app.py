34244ba897d0cb9cc4bf7e173a8b3f4985d6d6f8
DABWgithub <130961026+DABWgithub@users.noreply.github.com>
2023-04-17 12:08:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 
 import streamlit
 streamlit.title('My parents new Healthy Diner')
+
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omegea 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale , Spinash & Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free-Range Egg')
